1210f665b89d80784df89bac10cde06195e58594
AayushiAggarwal-7 <aayushiagg2007@gmail.com>
2026-09-12 20:40:53 +0530

    fix leaflet default marker icons not loading via Vite bundler

--- src/components/dashboard/MapView.jsx
+++ src/components/dashboard/MapView.jsx
@@ -1,9 +1,20 @@
 import { MapContainer, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import ParkBoundary from './ParkBoundary';
 import FloodRisk from './FloodRisk';
 import ESZOverlay from './ESZOverlay';
 import PoachingReports from './PoachingReports';
 import 'leaflet/dist/leaflet.css';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const MapView = ({ activeLayers, userRole }) => {
   return (
